5fb87209845769668e17e2de712bbbe3d4bbc8d7
srzh <smpetrov18@yandex.ru>
2020-06-18 15:35:11 +0400

    circle

--- src/edu/inst/WriteObject.java
+++ src/edu/inst/WriteObject.java
@@ -6,17 +6,16 @@ import java.io.ObjectOutputStream;
 
 public class WriteObject {
     public static void main(String[] args) {
-        Person person1 = new Person(1, "Bob");
-        Person person2 = new Person(2, "Mike");
+        Person[] people = {new Person (1, "Bob"), new Person (2, "Mike"),
+        new Person(3, "Tom")};
+
 
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-            oos.writeObject(person1);
-            oos.writeObject(person2);
-
-            oos.close();
+            oos.writeObject(people);
+            fos.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
